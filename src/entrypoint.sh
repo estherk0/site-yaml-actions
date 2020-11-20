f292b3b26bc5a46b6b5eeec788b5c3ad39c09615
Esther Kim <jabbukka@naver.com>
2020-11-20 15:17:34 +0900

    fix bug

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,8 +6,8 @@ set -o pipefail
 TARGET_DIR=$1
 OUTPUT_FILE=$2
 
-echo "### Run kustomize command <<<"
+echo "### Run kustomize command ###"
 echo ""
 echo ""
 
-bash -c "set -e;  set -o pipefail; kustomize build --enable_alpha_plugins $TARGET_DIR -o $OUTPUT_FILE"
+kustomize build --enable_alpha_plugins $TARGET_DIR -o $OUTPUT_FILE
