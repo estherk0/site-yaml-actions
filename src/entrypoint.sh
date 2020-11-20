8b32b31fdc47897ed35b5d5895b0b12c1676f11f
Esther Kim <jabbukka@naver.com>
2020-11-20 15:10:05 +0900

    add output files

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 set -o pipefail
 
 TARGET_DIR=$1
+OUTPUT_FILE=$2
 
 echo "### Run kustomize command <<<"
 echo ""
 echo ""
 
-bash -c "set -e;  set -o pipefail; kustomize build --enable_alpha_plugins $TARGET_DIR"
+bash -c "set -e;  set -o pipefail; kustomize build --enable_alpha_plugins $TARGET_DIR -o $OUTPUT_FILE"
